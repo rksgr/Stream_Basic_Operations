fb8ed40f4d006d07f8f8f08d4d684cf8f4714d2a
rksgr <r_kjha@yahoo.com>
2022-01-05 18:59:28 +0530

    Added: Welcome Message

--- TestStreamBasics.java
+++ TestStreamBasics.java
@@ -1,16 +1,8 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.ArrayList;
 
 public class TestStreamBasics {
+
     public static void main(String[] args) {
-        List<Integer> nums = Arrays.asList(2,3,4,5);
-        List<Integer> square = nums.stream().map(x -> x*x).collect(Collectors.toList());
-        System.out.println(square);
-        ArrayList<Integer> arr_int = new ArrayList<>();
-        arr_int.add(2);
-        arr_int.add(5);
-        System.out.println(arr_int.stream().collect(Collectors.toList()));
+        System.out.println("Welcome to BridgeLabzz!");
+        System.out.println("Today we shall perform basic operations on related to streams");
     }
 }
