f3b518456c4a5ef0058518cf80fa97eb13551a64
rksgr <r_kjha@yahoo.com>
2022-01-05 20:42:28 +0530

    Refactored: Stream Filter Peek Max Min operations

--- TestStreamBasics.java
+++ TestStreamBasics.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.ArrayList;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.*;
@@ -39,8 +39,34 @@ public class TestStreamBasics {
         List<Double> list_doub = strm_doub.collect(toList());
         list_doub.forEach(num -> System.out.println(num));
     }
+    /*
+    Use case 2.4: Filter the even numbers from the number stream and store the result
+     */
+    public void filterEvenStore(){
+        List<Integer> list_int = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        List<Integer> list_int_filt = list_int.stream().filter(num -> (num%2==0)).collect(toList());
+        list_int_filt.forEach(numm -> System.out.println(numm));
+    }
+    /*
+    Use case 2.5: Peek and show first even number in the number stream
+     */
+    public void peekShowFirstEven(){
+        List<Integer> list_int = Arrays.asList(1,2,3,4,5,6,7,8,9);
+        Optional<Integer> first_even_num = list_int.stream().filter(num -> num%2==0).peek(System.out::println).findFirst();
+    }
+    /*
+    Use case 2.6: Find min and max number in the number stream
+     */
+    public void findMaxMin(){
+        List<Integer> list_int = Arrays.asList(1,2,3,44,98,4,5,6,7,8,9);
+        Optional<Integer> max_num = list_int.stream().max((num1,num2)->num1.compareTo(num2));
+        System.out.println("Maximum number in stream: "+ max_num.get());
+        Optional<Integer> min_num = list_int.stream().min((num1,num2)->num1.compareTo(num2));
+        System.out.println("Minimum number in stream: " + min_num.get());
+    }
+
     public static void main(String[] args) {
         TestStreamBasics testbasic = new TestStreamBasics();
-        testbasic.transDoubleList();
+        testbasic.findMaxMin();
     }
 }
